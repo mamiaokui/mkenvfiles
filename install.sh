4d626f0a9c6243f8c5f26609c115519228315759
mamk <mamk@ucweb.com>
2014-06-30 17:36:10 +0800

    install more libs

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
- sudo apt-get install chromium-browser git virtualbox git-svn emacs cscope xcscope-el
+ sudo apt-get install chromium-browser git virtualbox git-svn emacs cscope xcscope-el ccache g++ gperf bison flex perl g++-multilib ant
+echo "add ~/mkenvfile/bin to PATH"
