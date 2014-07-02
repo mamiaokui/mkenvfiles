d55b37d5f73a4c5f3813300b0e280c839dff415b
mamk <mamk@ucweb.com>
2014-07-02 14:56:21 +0800

    add subversion

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
- sudo apt-get install chromium-browser git virtualbox git-svn emacs cscope xcscope-el ccache g++ gperf bison flex perl g++-multilib ant
+ sudo apt-get install chromium-browser git virtualbox git-svn emacs cscope xcscope-el ccache g++ gperf bison flex perl g++-multilib ant subversion
 echo "add ~/mkenvfile/bin to PATH"
 echo "add mount disk to configure file"
