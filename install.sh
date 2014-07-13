38cab2c4118e46afbd0114667ab3bfe23d3479d5
mamk <mamiaokui@gmail.com>
2014-07-13 11:29:00 +0800

    gnome-tweak-tool set caps as ctrl

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
  sudo apt-get install chromium-browser git virtualbox git-svn emacs cscope xcscope-el ccache g++ gperf bison flex perl g++-multilib ant subversion wmctrl
+
 echo "add ~/mkenvfile/bin to PATH"
 echo "add mount disk to configure file"
+echo "use gnome-tweak-tool to set caps as ctrl"
