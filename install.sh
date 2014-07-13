277612b616930f68d349a5ebd2fca3f107682744
mamk <mamiaokui@gmail.com>
2014-07-13 11:47:59 +0800

    add flash

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
- sudo apt-get install chromium-browser git virtualbox git-svn emacs cscope xcscope-el ccache g++ gperf bison flex perl g++-multilib ant subversion wmctrl
+ sudo apt-get install chromium-browser git virtualbox git-svn emacs cscope xcscope-el ccache g++ gperf bison flex perl g++-multilib ant subversion wmctrl  ttf-wqy-microhei ttf-wqy-zenhei xfonts-wqy pepperflashplugin-nonfree 
 
 echo "add ~/mkenvfile/bin to PATH"
 echo "add mount disk to configure file"
