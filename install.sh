d62bf1ec7d4a9842ce8a3a8eefc81d7983ff291a
mamk <mamiaokui@gmail.com>
2014-07-13 12:38:36 +0800

    wireshark

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
- sudo apt-get install chromium-browser git virtualbox git-svn emacs cscope xcscope-el ccache g++ gperf bison flex perl g++-multilib ant subversion wmctrl  ttf-wqy-microhei ttf-wqy-zenhei xfonts-wqy pepperflashplugin-nonfree 
+ sudo apt-get install chromium-browser git virtualbox git-svn emacs cscope xcscope-el ccache g++ gperf bison flex perl g++-multilib ant subversion wmctrl  ttf-wqy-microhei ttf-wqy-zenhei xfonts-wqy pepperflashplugin-nonfree wireshark
 
 echo "add ~/mkenvfile/bin to PATH"
 echo "add mount disk to configure file"
