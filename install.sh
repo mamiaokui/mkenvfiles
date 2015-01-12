384e7536b010b89fa067c7b160bfb788fb6c3f5b
mamk <mamk@gmail.com>
2015-01-12 14:33:08 +0800

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
- sudo apt-get install chromium-browser git virtualbox git-svn emacs cscope xcscope-el ccache g++ gperf bison flex perl g++-multilib ant subversion wmctrl  ttf-wqy-microhei ttf-wqy-zenhei xfonts-wqy pepperflashplugin-nonfree wireshark
+ sudo apt-get install chromium-browser git virtualbox git-svn emacs cscope xcscope-el ccache g++ gperf bison flex perl g++-multilib ant subversion wmctrl  ttf-wqy-microhei ttf-wqy-zenhei xfonts-wqy pepperflashplugin-nonfree wireshark apache2
 
-echo "add ~/mkenvfile/bin to PATH"
+echo "add ~/mkenvfiles/bin to PATH"
 echo "add mount disk to configure file"
 echo "use gnome-tweak-tool to set caps as ctrl"
 echo "if this is MAC and what to use gdb, can add s permission to gdb and chown it as root"
+echo "download sdk to ~/tools, add tools to PATH"
+echo "download ndk to ~/tools, add tools to PATH"
+echo "copy sdk 14 to sdk 8 and sdk 14"
+echo "set up ccache: ccache -M  50G and then export NDK_CCACHE=ccache"
+
+
