55a325720c4f174dd03169627a3e27fa84641afb
mamiaokui <mamiaokui@gmail.com>
2015-06-17 13:40:37 +0800

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 sudo apt-get install chromium-browser git virtualbox git-svn emacs cscope xcscope-el ccache g++ gperf bison flex perl g++-multilib ant subversion wmctrl  ttf-wqy-microhei ttf-wqy-zenhei xfonts-wqy pepperflashplugin-nonfree wireshark apache2 openjdk-7-jdk openjdk-7-jre meld eclipse gnome-tweak-tool
 git config --global diff.tool meld
 ccache -M  5G
+cp .emacs ~/
 echo "source ~/mkenvfiles/ext_bashrc" >> ~/.bashrc
 echo "add mount disk to configure file, sudo blkid /dev/sdb1, emacs /etc/fstab, UUID=c48d7a54-0af2-45cc-82fa-fe040b62e55d /home/mamk/ssd  ext4    defaults        0       2 "
 echo "use gnome-tweak-tool to set caps as ctrl"
